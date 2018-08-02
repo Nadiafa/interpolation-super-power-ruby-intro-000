afe7f5b7bfecbe3e39b5b9a250df60945dcb9e6d
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-02 15:48:02 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(array)
-  puts "#{array[0][0].upcase}: #{array[0]}, O: #{array[1]}, Y: #{array[2]}, G: #{array[3]}, B: #{array[4]}, I: #{array[5]}, V: #{array[6]}"
+  puts "#{array[0][0].upcase}: #{array[0]}, #{array[][0].upcase}: #{array[1]}, Y: #{array[2]}, G: #{array[3]}, B: #{array[4]}, I: #{array[5]}, V: #{array[6]}"
 end
